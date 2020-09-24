a4ac0f9c35983d187695ee16c17c28e9fbab0629
sebchiarelli <47152370+sebchiarelli@users.noreply.github.com>
2020-09-24 21:12:51 +0200

    Add files via upload

---
+++ js/portfolio.js
@@ -0,0 +1,83 @@
+var galleryIndex = 0;
+setInterval("change_background()", 9100);
+
+var divbg = document.querySelector(".background");
+
+var gallery = ["paugres", "sicile", "italie", "ciotat", "puydufou", "road_trip"];
+
+function change_background(){
+    
+    divbg.setAttribute("style", "background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url('images/"+ gallery[galleryIndex] +".jpg'); height: 100vh; background-size: cover;");         
+    galleryIndex++;
+    if(galleryIndex > 5){
+        galleryIndex = 0;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*    switch (x) {
+
+        case 0:
+            divbg.setAttribute("style", "background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url('images/paugres.jpg'); height: 100vh; background-size: cover;");
+            
+            break;
+    
+        case 1:
+            divbg.setAttribute("style", "background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url('images/sicile.jpg'); height: 100vh; background-size: cover;");
+            
+            break;
+    
+        case 2:
+            divbg.setAttribute("style", "background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url('images/italie.jpg'); height: 100vh; background-size: cover;");
+            
+            break;
+    
+        case 3:
+            divbg.setAttribute("style", "background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url('images/ciotat.jpg'); height: 100vh; background-size: cover;");
+            
+            break;
+    
+        case 4:
+            divbg.setAttribute("style", "background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url('images/puydufou.jpg'); height: 100vh; background-size: cover;");
+            
+            break; 
+    
+        default:
+            x = 0;
+            divbg.setAttribute("style", "background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url('images/road_trip.jpg'); height: 100vh; background-size: cover;");
+            break; 
+    }    */
+
