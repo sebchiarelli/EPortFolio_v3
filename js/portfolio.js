2e0060e3ce6ec5778378bd62a89369cdc1857fba
sebchiarelli <47152370+sebchiarelli@users.noreply.github.com>
2020-09-25 19:14:47 +0200

    Add files via upload

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,22 +1,25 @@
 var galleryIndex = 0;
-setInterval("change_background()", 9100);
+setInterval("change_background()", 12000);
 
 var divbg = document.querySelector(".background");
 
-var gallery = ["paugres", "sicile", "italie", "ciotat", "puydufou", "road_trip"];
+var gallery = ["puydufou", "ciotat", "road_trip"];
+var nav = document.querySelector("nav");
 
 function change_background(){
     
     divbg.setAttribute("style", "background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url('images/"+ gallery[galleryIndex] +".jpg'); height: 100vh; background-size: cover;");         
     galleryIndex++;
-    if(galleryIndex > 5){
+    if(galleryIndex > 2){
         galleryIndex = 0;
     }
 }
 
+window.addEventListener('scroll',goingdown);
 
-
-
+function goingdown(){
+    nav.classList.add("scrollnav");
+}
 
 
 
